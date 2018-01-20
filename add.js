030ce62bcfb3e724fae978782399423186935454
Richard Samuelsson <richard.samuelsson@redeye.se>
2018-01-20 11:57:21 +0100

    rework

--- add.js
+++ add.js
@@ -1,278 +1,42 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var npm = require('npm');
-var raw = false;
+const fs = require('fs');
+const path = require('path');
+const args = process.argv.slice(2);
 
-var self = require("./package.json");
+let dependencyName = 'dependencies';
 
-var svr = /^v{0,1}([0-9]+\.{0,1}){1,3}(\-([a-z0-9]+\.{0,1})+){0,1}(\+(build\.{0,1}){0,1}([a-z0-9]+\.{0,1}){0,}){0,1}$/;
+const deps = [];
 
-// stop npm from polluting the commandline
-process.stderr.write = new Function;
+args.forEach(dep => {
 
-var sign = [
-		"         ___ ____ __ __",
-	   	"        / _ `/ _ \\\\ \\ /",
-	   	"        \\_,_/ .__/_\\_\\ ",
-	   	"           /_/\n\t\t\033[1mdepadd " + self.version, "\033[0m",
-	   	"\tCLI for adding deps to a npm module."
-	].join("\n");
-
-var help = sign + [
-	"", "",
-	"Usage: ",
-	"\tadddep <packages> [options]", "", "",
-	"Options:",
-	"\t-h, --help\tHelp screen",
-	"\t-v, --version\tCurrent version",
-	"\t-i, --install\tInstall packages after adding them",
-	"\t-b, --bump\tUpdate the version of a module (valid semver must be specified)"
-].join("\n");
-
-var version = self.version;
-
-if (process.argv[0] == 'node') {
-	argv = process.argv.slice(2);
-} else {
-	argv = process.argv;
-}
-
-var log = function () {
-	arguments = Array.prototype.slice.call(arguments);
-	var bold = arguments[arguments.length - 1] === -0xA0; if(bold)arguments=arguments.slice(0, arguments.length-1);
-	process.stdout.write.call(process.stdout, (bold?"\033[1m":"")+(arguments.length ? arguments.concat(["\n"]) : ["\n"]).join(" ")+(bold?"\033[0m":""));
-};
-
-console.log = new Function;
-
-var exit = function (message, error) {
-	process.stdout.write(message + "\n\n");
-	err = error || null;
-	process.exit(err);
-}
-
-var _install = function () {
-	npm.load({}, function(err) {
-		npm.commands.install([], function(err, rd) {})
-	})
-}
-
-// semver expand
-var sve = function (ver) {
-	var vsl = String(ver).split(".").length;
-
-	// 1 => 1.0.0
-	if ( vsl === 1 ) {
-		ver = ver + ".0.0";
-	} else
-	// 1.1 => 1.1.0
-	if ( vsl === 2 ) {
-		ver = ver + ".0";
-	}
-
-	return ver;
-}
-
-var base = process.cwd();
-
-var main = function(deps, install) {
-	try {
-		var package = JSON.parse(fs.readFileSync(base + "/package.json"));
-	} catch(e) {
-		exit("Inexistent or malformed package.json.");
-	}
-	
-	var i = 0;
-
-	var _next = function () {
-		if ( ++i !== deps.length ) return;
-
-		fs.writeFileSync(base + '/package.json', JSON.stringify(package, true, 2));
-		log("\n» Updated package.json\n");
-	}
-	
-	if (raw) {
-		deps.forEach(function (pck) {
-			var wc = pck[pck.length-1] === "*";
-			var ver;
-
-			if (wc) {
-				pck = pck.slice(0, pck.length-1);
-			} else {
-				var lI = pck.lastIndexOf("v");
-				var sx = pck.slice(lI + 1);
-
-				if (svr.test(sx)) {
-					ver = sve(sx);
-					pck = pck.slice(0, lI)
-				}
-			}
-			package.dependencies[pck] = wc ? "*" : ver;
-			log("» Adding: ", pck + " v" + package.dependencies[pck])
-			return _next();
-		});
+	if (dep.indexOf('-D') !== -1) {
+		dependencyName = 'devDependencies';
 		return;
 	}
-	npm.load({}, function(err) {
 
-
-		deps.forEach(function (pck) {
-			// wildcard: version will be *
-			var wc = pck[pck.length-1] === "*";
-			var ver;
-
-			if (wc) {
-				pck = pck.slice(0, pck.length-1);
-			} else {
-				var lI = pck.lastIndexOf("v");
-				var sx = pck.slice(lI + 1);
-
-				if (svr.test(sx)) {
-					ver = sve(sx);
-					pck = pck.slice(0, lI)
-				}
-			}
-
-			// force: don't check npm - requires wildcard or version
-			var fc = pck[0] === "@" && (wc || ver);
-
-			if (fc) pck = pck.slice(1);
-
-			var del = pck[0] === "-";
-
-			if (del) pck = pck.slice(1);
-
-			package.dependencies = package.dependencies || {};
-
-			if ( package.dependencies instanceof Array )
-				package.dependencies = {};
-
-			console.log("fc", fc, "wc", wc, "ver", ver, "del", "del")
-
-			if (del) {
-				if (package.dependencies[pck]) {
-					delete package.dependencies[pck];
-
-					log("» Deleting: " + pck);
-				} else {
-					return log("» Couldn't locate " + pck + " in package.. ignoring.", -0xA0);
-				}
-
-				return _next();
-			}
-
-			if ( fc ) {
-				package.dependencies[pck] = wc ? "*" : ver;
-
-				log("» Adding: ", pck + " v" + package.dependencies[pck])
-
-				return _next();
-			} else {
-				npm.commands.show([pck, 'name'], function(err, mod) {
-					if (err) return log("« Warning: couldn't locate " + pck + ".. omitting it.", -0xA0), _next();
-
-					// get top-most version
-					var version = wc ? "*" : Object.keys(mod)[0];
-
-					ver && log(
-						"« Warning: Specified version " + ver + ", but didn't force it. (use @" + pck + "v" + ver + ")\n" +
-						"« Warning: Using upstream " + version + " instead."
-					, -0xA0);
-
-					package.dependencies[pck] = version;
-
-					log("» Adding: ", pck + " v" + package.dependencies[pck])
-
-					return _next();
-				});
-			}
-		});
+	const [name, version] = dep.split('@');
+	deps.push({
+		name,
+		version
 	});
-	if (install == true) _install()
 
+}); 
+
+const pkgPath = path.resolve(process.cwd(), 'package.json');
+const rawPkg = fs.readFileSync(pkgPath)
+const pkg = JSON.parse(rawPkg);
+
+if (!pkg[dependencyName]) {
+	pkg[dependencyName] = {};
 }
 
-var xa = [], xo = [];
+deps.forEach(({name, version}) => {
 
-var ops = [
-	"-h", "--help",
-	"-v", "--version",
-	"-i", "--install",
-	"-b", "--bump"
-]
+	pkg[dependencyName][name] = version;
 
-argv.forEach(function (v) {
-	return ~ops.indexOf(v) ? xo.push(v) : xa.push(v);
-})
+});
 
-if (require.main === module) {
-	var install, stop;
+const pkgString = JSON.stringify(pkg, true, 2);
 
-	xo.forEach(function () {
-		if ( ~xo.indexOf("-h") || ~xo.indexOf("--help") ) {
-			stop = true;
-			return exit(help);
-		}
-
-		if ( ~xo.indexOf("-v") || ~xo.indexOf("--version") ) {
-			stop = true;
-			return exit(sign);
-		}
-
-		if ( ~xo.indexOf("-i") || ~xo.indexOf("--install") ) {
-			install = true;
-		}
-		if ( ~xo.indexOf("-r") || ~xo.indexOf("--raw") ) {
-			raw = true;
-		}
-
-		if ( xo[0] === "-b" || xo[0] === "-bump" ) {
-			log(sign);
-
-			if (svr.test(xa[0])) {
-				var ver = sve(xa[0]);
-
-				try {
-					var package = JSON.parse(fs.readFileSync(base + "/package.json"));
-				} catch(e) {
-					return exit("\n« Warning: Bump failed, inexistent or malformed package.json.");
-				}
-
-				if ( package.version === ver ) {
-					log(
-						"\n« Bumping: Version didn't change, aborting.\n"
-					, -0xA0);
-
-					return process.reallyExit();
-				} else {
-					log(
-						"\n« Bumping: Updating from " + package.version + " to " + ver
-					, -0xA0);
-
-					package.version = ver;
-
-					fs.writeFileSync(base + '/package.json', JSON.stringify(package, true, 2));
-					log("\n» Updated package.json");
-
-					return process.reallyExit();
-				}
-
-			} else {
-				log(
-					"\n\t« Warning: Trying to bump version but specified version is invalid. (" + (xa[0] ? xa[0] : "none given") + ")\n"
-				, -0xA0);
-
-				return process.reallyExit();
-			}
-		}
-	})
-
-	if (!xa.length && !xo.length) {
-		exit(help)
-	} else {
-		log(sign + "\n")
-	}
-
-	!stop && main(xa, install);
-}
+fs.writeFileSync(pkgPath, pkgString);
